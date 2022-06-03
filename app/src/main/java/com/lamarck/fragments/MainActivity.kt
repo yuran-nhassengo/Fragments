8fc4f262cb785d122790edeec029f423ce271c7b
yuran-nhassengo <yurannhassengo@gmail.com>
2022-06-03 19:37:05 +0200

    Add a fragment programmatically

--- app/src/main/java/com/lamarck/fragments/MainActivity.kt
+++ app/src/main/java/com/lamarck/fragments/MainActivity.kt
@@ -2,10 +2,19 @@ package com.lamarck.fragments
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if(savedInstanceState ==null){
+            supportFragmentManager.commit {
+                setReorderingAllowed(true)
+                add<exemplo_fragment>(R.id.fragment_container_view)
+            }
+        }
     }
 }
